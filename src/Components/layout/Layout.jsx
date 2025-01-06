35acf9827f05928fd14caded182bb3a2261533c5
karimhani1 <karimhani1111@gmail.com>
2025-01-06 21:00:03 +0200

    Fixed imports

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../navbar/navbar'
-import Footer from '../footer/footer'
+import Navbar from '../navbar/Navbar'
+import Footer from '../footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 
